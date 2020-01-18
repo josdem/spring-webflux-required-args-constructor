12ca43af86e71c3a271396b09d7370b1c5ce6bfa
josdem <joseluis.delacruz@gmail.com>
2020-01-18 16:58:10 -0500

    Adding test case

---
+++ src/test/java/com/jos/dem/spring/webflux/lombok/PersonControllerTest.java
@@ -0,0 +1,36 @@
+package com.jos.dem.spring.webflux.lombok;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.jos.dem.spring.webflux.lombok.model.Person;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+class PersonControllerTest {
+
+	@Autowired
+	private WebTestClient webTestClient;
+
+	@Test
+	@DisplayName("Should get all persons")
+	void shouldGetAllPersons() {
+		webTestClient.get()
+				.uri("/persons/")
+				.exchange()
+				.expectStatus().isOk()
+				.expectBodyList(Person.class)
+				.value(persons -> {
+					assertTrue(persons.contains(new Person("josdem", "joseluis.delacruz@gmail.com")), "should contain josdem");
+					assertTrue(persons.contains(new Person("tgrip", "tgrip@email.com")), "should contain tgrip");
+					assertTrue(persons.contains(new Person("edzero", "edzero@email.com")), "should contain edzero");
+					assertTrue(persons.contains(new Person("skuarch", "skuarch@email.com")), "should contain skuarch");
+					assertTrue(persons.contains(new Person("jeduan", "jeduan@email.com")), "should contain jeduan");
+				});
+	}
+
+}
